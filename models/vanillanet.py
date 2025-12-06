86cb106675b60216156bf199feb1755cb6615885
Dorin Flocos <dflocos@gmail.com>
2025-12-05 20:02:37 -0500

    implement progressive activations with grouped convolutions

--- models/vanillanet.py
+++ models/vanillanet.py
@@ -28,7 +28,7 @@ class activation(nn.ReLU):
     def forward(self, x):
         if self.deploy:
             return torch.nn.functional.conv2d(
-                super(activation, self).forward(x), 
+                super(activation, self).forward(x),
                 self.weight, self.bias, padding=self.act_num, groups=self.dim)
         else:
             return self.bn(torch.nn.functional.conv2d(
@@ -45,7 +45,53 @@ class activation(nn.ReLU):
         std = (running_var + eps).sqrt()
         t = (gamma / std).reshape(-1, 1, 1, 1)
         return kernel * t, beta + (0 - running_mean) * gamma / std
-    
+
+    def switch_to_deploy(self):
+        kernel, bias = self._fuse_bn_tensor(self.weight, self.bn)
+        self.weight.data = kernel
+        self.bias = torch.nn.Parameter(torch.zeros(self.dim))
+        self.bias.data = bias
+        self.__delattr__('bn')
+        self.deploy = True
+
+
+class progressive_activation(nn.ReLU):
+    def __init__(self, dim, act_num=3, deploy=False, groups=1):
+        super(progressive_activation, self).__init__()
+        self.act_num = act_num
+        self.deploy = deploy
+        self.dim = dim
+        assert groups >= 1 and dim % groups == 0
+        self.groups = groups
+        self.weight = torch.nn.Parameter(torch.randn(dim, dim // groups, act_num*2 + 1, act_num*2 + 1))
+        if deploy:
+            self.bias = torch.nn.Parameter(torch.zeros(dim))
+        else:
+            self.bias = None
+            self.bn = nn.BatchNorm2d(dim, eps=1e-6)
+        weight_init.trunc_normal_(self.weight, std=.02)
+
+    def forward(self, x):
+        if self.deploy:
+            return torch.nn.functional.conv2d(
+                super(progressive_activation, self).forward(x),
+                self.weight, self.bias, padding=self.act_num, groups=self.groups)
+        else:
+            return self.bn(torch.nn.functional.conv2d(
+                super(progressive_activation, self).forward(x),
+                self.weight, padding=self.act_num, groups=self.groups))
+
+    def _fuse_bn_tensor(self, weight, bn):
+        kernel = weight
+        running_mean = bn.running_mean
+        running_var = bn.running_var
+        gamma = bn.weight
+        beta = bn.bias
+        eps = bn.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta + (0 - running_mean) * gamma / std
+
     def switch_to_deploy(self):
         kernel, bias = self._fuse_bn_tensor(self.weight, self.bn)
         self.weight.data = kernel
@@ -119,6 +165,73 @@ class Block(nn.Module):
         self.__delattr__('conv2')
         self.act.switch_to_deploy()
         self.deploy = True
+
+
+class EfficientBlock(nn.Module):
+    def __init__(self, dim, dim_out, act_num=3, stride=2, deploy=False, groups=1, stage_idx=0):
+        super().__init__()
+        self.act_learn = 1
+        self.deploy = deploy
+        self.act_learn = 1
+        assert dim_out % groups == 0 and dim % groups == 0
+        self.groups = groups
+        progressive_act_num = act_num + stage_idx
+        if self.deploy:
+            self.conv = nn.Conv2d(dim, dim_out, kernel_size=1)
+        else:
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(dim, dim, kernel_size=1),
+                nn.BatchNorm2d(dim, eps=1e-6),
+            )
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(dim, dim_out, kernel_size=1),
+                nn.BatchNorm2d(dim_out, eps=1e-6)
+            )
+
+        self.pool = nn.Identity() if stride == 1 else nn.MaxPool2d(stride)
+        self.act = progressive_activation(dim_out, progressive_act_num, deploy=self.deploy, groups=self.groups)
+
+    def forward(self, x):
+        if self.deploy:
+            x = self.conv(x)
+        else:
+            x = self.conv1(x)
+
+            # We use leakyrelu to implement the deep training technique.
+            x = torch.nn.functional.leaky_relu(x, self.act_learn)
+
+            x = self.conv2(x)
+
+        x = self.pool(x)
+        x = self.act(x)
+        return x
+
+    def _fuse_bn_tensor(self, conv, bn):
+        kernel = conv.weight
+        bias = conv.bias
+        running_mean = bn.running_mean
+        running_var = bn.running_var
+        gamma = bn.weight
+        beta = bn.bias
+        eps = bn.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta + (bias - running_mean) * gamma / std
+
+    def switch_to_deploy(self):
+        kernel, bias = self._fuse_bn_tensor(self.conv1[0], self.conv1[1])
+        self.conv1[0].weight.data = kernel
+        self.conv1[0].bias.data = bias
+        # kernel, bias = self.conv2[0].weight.data, self.conv2[0].bias.data
+        kernel, bias = self._fuse_bn_tensor(self.conv2[0], self.conv2[1])
+        self.conv = self.conv2[0]
+        self.conv.weight.data = torch.matmul(kernel.transpose(1,3), self.conv1[0].weight.data.squeeze(3).squeeze(2)).transpose(1,3)
+        self.conv.bias.data = bias + (self.conv1[0].bias.data.view(1,-1,1,1)*kernel).sum(3).sum(2).sum(1)
+        self.__delattr__('conv1')
+        self.__delattr__('conv2')
+        self.act.switch_to_deploy()
+        self.deploy = True
+
     
 
 class VanillaNet(nn.Module):
@@ -145,10 +258,12 @@ class VanillaNet(nn.Module):
 
         self.act_learn = 1
 
+        group_config = [16, 32, 64, 128]
         self.stages = nn.ModuleList()
         for i in range(len(strides)):
             if not ada_pool:
-                stage = Block(dim=dims[i], dim_out=dims[i+1], act_num=act_num, stride=strides[i], deploy=deploy)
+                # stage = Block(dim=dims[i], dim_out=dims[i+1], act_num=act_num, stride=strides[i], deploy=deploy)
+                stage = EfficientBlock(dim=dims[i], dim_out=dims[i+1], act_num=act_num, stride=strides[i], deploy=deploy, groups=group_config[i], stage_idx=i)
             else:
                 stage = Block(dim=dims[i], dim_out=dims[i+1], act_num=act_num, stride=strides[i], deploy=deploy, ada_pool=ada_pool[i])
             self.stages.append(stage)
